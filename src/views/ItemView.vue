e2dd22fb4a93a240bddd24ead74c03930beee775
YevhenKataiev <kataev.evgen.1989@gmail.com>
2025-01-09 17:10:04 +0200

    create app structure

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,12 +1,15 @@
+<script setup lang="ts">
+import ProfileCard from '@/components/ProfileCard.vue';
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="wrap">
+    <ProfileCard />
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
+  .wrap {
     min-height: 100vh;
     display: flex;
     align-items: center;
