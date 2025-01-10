da3110c9666d0004a325f3a418ff5f327352be13
YevhenKataiev <kataev.evgen.1989@gmail.com>
2025-01-10 19:31:55 +0200

    create usercard template

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,10 @@ import { RouterView } from 'vue-router'
   </router-view>
 </template>
 
-<style scoped>
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.5s ease;
-}
+<style lang="sass" scoped>
+.v-enter-active, .v-leave-active
+  transition: opacity 0.5s ease
 
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
+.v-enter-from, .v-leave-to
+  opacity: 0
 </style>
--- src/components/user/UserList.vue
+++ src/components/user/UserList.vue
@@ -5,6 +5,7 @@ import { useUser } from '@/composables/useUser';
 import { computed, onMounted, ref } from 'vue';
 import Loader from '../global/Loader.vue';
 import Filter from '../global/Filter.vue';
+import UserCard from './UserCard.vue';
 
 const userStore = useUserStore();
 const { userList, updateUserList } = userStore;
@@ -14,19 +15,31 @@ const list = ref<IUser[]>(userList);
 const filteredList = computed(() => list.value.filter(user => user.name.toLowerCase().includes(search.value)))
 const search = ref<string>('');
 const fetchlist = async () => {
-	await fetchData();
-	updateUserList(data.value as IUser[]);
-	list.value = data.value as IUser[]
+  await fetchData();
+  updateUserList(data.value as IUser[]);
+  list.value = data.value as IUser[]
 };
 onMounted(() => { fetchlist() })
 </script>
 <template>
-	<Loader v-if="loading" />
-	<div v-else>
-		<Filter v-model="search" />
-		<div v-for="user in filteredList" :key="user.id">
-			{{ user }}
-		</div>
-	</div>
+  <Loader v-if="loading" />
+  <div v-else>
+    <Filter v-model="search" />
+    <div class="container">
+      <UserCard v-for="user in filteredList" :key="user.id" :user="user" />
+    </div>
+  </div>
 
 </template>
+<style lang="sass" scoped>
+.container
+	display: grid
+	grid-template-columns: 1fr
+	gap: 1rem
+
+
+@media (min-width: 480px)
+	.container
+		grid-template-columns: 1fr 1fr
+
+</style>
