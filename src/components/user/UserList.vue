c7396dcc7bd7677c6c95a9855866dbe94ee95cad
YevhenKataiev <kataev.evgen.1989@gmail.com>
2025-01-10 17:18:06 +0200

    create fetch logic for list

--- src/components/user/UserList.vue
+++ src/components/user/UserList.vue
@@ -1,5 +1,29 @@
 <script setup lang="ts">
+import type { IUser } from '@/types/user.types';
+import { useUserStore } from '@/stores/users';
+import { useUser } from '@/composables/useUser';
+import { onMounted, ref } from 'vue';
+import Loader from '../global/Loader.vue';
+import Filter from '../global/Filter.vue';
+
+const userStore = useUserStore();
+const { userList, updateUserList } = userStore;
+const { getUserList } = useUser();
+const { data, loading, fetchData } = getUserList();
+const list = ref<IUser[]>(userList);
+const search = ref<string>('');
+const fetchlist = async () => {
+	await fetchData();
+	updateUserList(data.value as IUser[]);
+	list.value = data.value as IUser[]
+};
+onMounted(() => { fetchlist() })
 </script>
 <template>
-    list
+	<Loader v-if="loading" />
+	<div v-else>
+		<Filter />
+		<pre>{{ list }}</pre>
+	</div>
+
 </template>
